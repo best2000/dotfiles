68a8e5cba2dd58e7397c98def870f6219ac7027a
best2000 <silentman@hotmail.co.th>
2024-02-24 03:14:56 +0700

    up

--- config.fish
+++ config.fish
@@ -1,5 +1,23 @@
+#set alias
 alias bat "batcat"
+alias ls "exa"
+alias ll "ls -lah --tree"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+end
+
+#cd to directory by fzf
+function cdfz 
+    cd $(find * -type d | fzf) 
+end
+
+#batcat file from fzf
+function bafz 
+    batcat $(find * -type f | fzf) 
+end
+
+#replace ll with exa
+function ll 
+    exa -lah --icons 
 end
--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-#setup fish
+#ref https://github.com/craftzdog/dotfiles-public
+#this setup is based on Ubuntu
+
+sudo apt update
+sudo apt upgrade
+
+#install tools
+sudo apt install fzf    #https://github.com/junegunn/fzf
+sudo apt install bat    #https://github.com/sharkdp/bat
+sudo apt install lsd    #https://github.com/ogham/exa?tab=readme-ov-file    #newer eza => https://github.com/eza-community/eza or lsd => https://github.com/lsd-rs/lsd
+
+#setup fish shell
 #https://github.com/fish-shell/fish-shell
 #install
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt upgrade
 sudo apt install fish
 #config
 cp -f ./config.fish ~/.config/fish/config.fish
@@ -13,13 +23,15 @@ cp -f ./fish_variables ~/.config/fish/config.fish
 #set default shell
 chsh -s $(which fish)
 
-#setup starship
+#setup starship prompt
 # https://starship.rs/
 #install
 curl -sS https://starship.rs/install.sh | sh
-echo -e 'eval "$(starship init zsh)"' >> ~/.config/fish/config.fish
+echo -e 'starship init fish | source' >> ~/.config/fish/config.fish
 #config
 mkdir -p ~/.config
 cp ./starship.toml ~/.config/starship.toml
 
 exec $SHELL
+
+#fish web config run => 'fish_config'
