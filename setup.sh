2f2578ded166a165d670615e735284d4da1b0f89
EmptyMan_ <silentman@hotmail.co.th>
2024-02-26 23:55:17 +0700

    fix

--- setup.sh
+++ setup.sh
@@ -4,34 +4,36 @@
 #this setup is based on Ubuntu
 
 sudo apt update
-apt upgrade
+sudo apt upgrade
 
 #install tools
-apt install neofetch
-apt install fzf    #https://github.com/junegunn/fzf
-apt install bat    #https://github.com/sharkdp/bat
-apt install exa    #https://github.com/ogham/exa?tab=readme-ov-file    #newer eza => https://github.com/eza-community/eza or lsd => https://github.com/lsd-rs/lsd
+sudo apt install neofetch
+sudo apt install fzf    #https://github.com/junegunn/fzf
+sudo apt install bat    #https://github.com/sharkdp/bat
+sudo apt install exa    #https://github.com/ogham/exa?tab=readme-ov-file    #newer eza => https://github.com/eza-community/eza or lsd => https://github.com/lsd-rs/lsd
 
 
 #setup neofetch
+neofetch && sleep 1
 cp -f ./neofetch/config.conf ~/.config/neofetch/config.conf
 
 #setup fish shell
 #https://github.com/fish-shell/fish-shell
 #install
-apt-add-repository ppa:fish-shell/release-3
-apt update
-apt install fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+exec fish && sleep 1
 #config
 cp -f ./fish/config.fish ~/.config/fish/config.fish
 cp -f ./fish/fish_variables ~/.config/fish/config.fish
 #set default shell
-chsh -s $(which fish)
+sudo chsh -s $(which fish)
 
 #setup starship prompt
 # https://starship.rs/
 #install
-curl -sS https://starship.rs/install.sh | sh
+sudo curl -sS https://starship.rs/install.sh | sh
 echo -e 'starship init fish | source' >> ~/.config/fish/config.fish
 #config
 mkdir -p ~/.config
